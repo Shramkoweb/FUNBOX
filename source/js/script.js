0a86db378b2722b8f4a156138a588eaf482748b2
Shramkoweb <shramko.web@yahoo.com>
2018-11-02 14:20:29 +0200

    Добавил интерактивные состояния для карточек

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,19 @@
-var card = document.querySelectorAll(".catalog__card");
+var card = document.querySelectorAll('.catalog__card');
 
 if (card.length > 0) {
   for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener("click", function () {
-      card[i].classList.toggle("catalog__card--selected");
+    card[i].addEventListener('click', function () {
+      card[i].classList.toggle('catalog__card--selected');
+    })
+  };
+};
+
+var link = document.querySelectorAll('.catalog-item__link');
+
+if (link.length > 0) {
+  for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function () {
+      card[i].classList.toggle('catalog__card--selected');
     })
   };
 };
